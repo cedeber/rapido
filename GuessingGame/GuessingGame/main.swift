feefb0c850943c1237ecefbc353045dbd8b5cc1d
Cédric Eberhardt <hello+code@cedeber.fr>
2020-11-22 10:45:22 +0100

    SwiftFormat

--- GuessingGame/GuessingGame/main.swift
+++ GuessingGame/GuessingGame/main.swift
@@ -2,26 +2,26 @@ import Foundation
 
 print("Guess the number!")
 
-let secretNumber = Int.random(in: 1..<100)
+let secretNumber = Int.random(in: 1 ..< 100)
 var guess: Int
 
 while true {
     print("Please input your guess.")
-    
+
     let input = readLine()?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
-    
+
     if let input = Int(input) {
         guess = input
     } else {
         continue
     }
-    
+
     print("You guessed \(guess)")
-    
+
     if guess < secretNumber {
         print("Too small!")
     } else if guess > secretNumber {
-        print ("Too big!")
+        print("Too big!")
     } else {
         print("You win!")
         break
