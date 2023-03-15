86ffbb7bc0a551fabe78189728f0ac8b3bc2bb11
Cédric Eberhardt <hello+code@cedeber.fr>
2023-03-15 20:18:20 +0100

    Add Guessing Game

---
+++ GuessingGame/GuessingGame/main.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+print("Guess the number!")
+
+let secretNumber = Int.random(in: 1 ..< 100)
+var guess: Int
+
+while true {
+    print("Please input your guess.")
+
+    let input = readLine()?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+
+    if let input = Int(input) {
+        guess = input
+    } else {
+        continue
+    }
+
+    print("You guessed \(guess)")
+
+    if guess < secretNumber {
+        print("Too small!")
+    } else if guess > secretNumber {
+        print("Too big!")
+    } else {
+        print("You win!")
+        break
+    }
+}
